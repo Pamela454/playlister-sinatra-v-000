a2602501d3e6b2e717406af26ff9b56ca725a263
Pamela Frieze <pfrieze@gmail.com>
2018-07-24 20:13:32 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,6 +38,6 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
-end
+  end
 
 end

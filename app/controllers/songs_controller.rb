69c5733de20509866073dcc338d2b8e30eef812d
Pamela Frieze <pfrieze@gmail.com>
2018-07-24 20:40:15 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -40,6 +40,7 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
     @song.save
 
+    flash[:message] = "Successfully updated song."
     redirect("/songs/#{@song.slug}")
   end
 

03dd64e32d2fcb9d159e50681772aa246fa28184
Pamela Frieze <pfrieze@gmail.com>
2018-07-24 20:34:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ require 'rack-flash'
 require 'sinatra/base'
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  use Rack::Flash
 
   enable :sessions
   set :session_secret, "my_application_secret"
